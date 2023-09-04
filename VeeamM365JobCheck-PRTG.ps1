5e22c83f771b5e25577865319d00e88f89ebf1da
Andreas Bucher <andreas.bucher@gmx.ch>
2023-09-04 09:12:25 +0200

    Initial commit

---
+++ VeeamM365JobCheck-PRTG.ps1
@@ -0,0 +1,118 @@
+﻿<#
+    .SYNOPSIS
+    This script opens a PS-Drive to check for XML files created through VeeamM365JobCheck-XML.ps1.
+    This Script is meant to be used as PRTG custom sensor.
+
+    .PARAMETER HostName
+    HostName where the XML is located.
+    This parameter is mandantory.
+
+    .PARAMETER JobName
+    JobName of the Veeam for Microsoft 365 backupjob to be monitored
+    This parameter is mandantory.
+
+    .PARAMETER UserName
+    UserName to connect to the server where the xml is located
+    This parameter is optional.
+
+    .PARAMETER Password
+    Password to connect to the server where the xml is located
+    This parameter is optional.
+
+    .INPUTS
+    None
+
+    .OUTPUTS
+    This script retrives an xml file and parses it to PRTG
+
+    .LINK
+    https://raw.githubusercontent.com/tn-ict/Public/master/Disclaimer/DISCLAIMER
+
+    .NOTES
+    Author  : Andreas Bucher
+    Version : 0.9.1
+    Purpose : XML-Part of the PRTG-Sensor VeeamM365JobCheck
+
+    .EXAMPLE
+    Create a Sensor in PRTG with the parameters as following
+    -HostName '%host' -JobName 'Jobname' -UserName '%windowsdomain\%windowsuser' -Password '%windowspassword'
+    Those %-parameters are retreived from the PRTG WebGUI
+
+    Try it standalone
+    .\VeeamM365JobCheck-PRTG.ps1 -JobName "Jobname" -HostName "Host" -UserName "domain\username" -Password "password"
+
+#>
+#----------------------------------------------------------[Declarations]----------------------------------------------------------
+# Declare input parameters
+Param(
+    [Parameter(Mandatory=$true)]
+    [string]$HostName,
+    [Parameter(Mandatory=$true)]
+    [string]$JobName,
+    [string]$UserName,
+    [string]$Password
+    )
+
+# Encoding auf UTF-8 stellen
+$PSDefaultParameterValues['*:Encoding'] = 'utf8'
+[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+[cultureinfo]::CurrentUICulture = 'de-CH'
+
+# Variables
+$rootpath  = "\\$HostName\c$\Temp\VeeamResults"
+$resultxml = "$JobName.xml"
+$nl        = [Environment]::NewLine
+#-----------------------------------------------------------[Functions]------------------------------------------------------------
+# Return Error-XML
+function Set-ErrorXML {
+    param(
+        $msg
+    )
+
+    $ErrorXML  = ""
+    $ErrorXML += '<?xml version="1.0" encoding="UTF-8" ?>' + $nl
+    $ErrorXML += "<prtg>" + $nl
+    $ErrorXML += "<error>1</error>" + $nl
+    $ErrorXML += "<text>$msg</text>" + $nl
+    $ErrorXML += "</prtg>" + $nl
+
+    return $ErrorXML
+    exit 1
+}
+#-----------------------------------------------------------[Execute]------------------------------------------------------------
+# Check if HostName was passed
+if ( -NOT "$HostName" ) { Set-ErrorXML "Kein Hostname als Parameter definiert" }
+
+# Use SMB if UserName and Password were not passed. Assume the servers are Members of the same domain
+if ( -NOT ($UserName -AND $Password) ) { $xmlResultPath = "$rootpath" }
+
+# Use a PowerShell Drive if username and password were passed
+elseif ( $UserName -AND $Password )
+{
+    $secpassword = ConvertTo-SecureString $Password -AsPlainText -Force
+    $cred = New-Object System.Management.Automation.PSCredential ($UserName, $secpassword)
+
+    New-PSDrive -Credential $cred -Name PS-VeeamResults -PSProvider FileSystem -Root $rootpath
+    $xmlResultPath = "PS-VeeamResults:"
+}
+
+# Throw an error if the above statements are not met
+else { Set-ErrorXML "Server $HostName nicht erreichbar. Benutzer- & Passwort Parameter prüfen..." }
+
+# Throw an error if the result path is not reachable
+if ( -NOT (Test-Path -Path "$xmlResultPath")) { Set-ErrorXML "Share $xmlResultPath nicht erreichbar..." }
+
+# Throw error if the result xml is not present
+elseif ( -NOT (Test-Path -Path "$xmlResultPath\$resultxml")) { Set-ErrorXML "$resultxml auf Share $xmlResultPath nicht vorhanden..." }
+
+# Throw an error if the result xml is older than 6 hours
+elseif ( -NOT (Test-Path -Path "$xmlResultPath\$resultxml" -NewerThan (Get-Date).AddHours(-6) )) { Set-ErrorXML "$resultxml auf Share $xmlResultPath älter als 36h, Task Scheduler prüfen." }
+
+# You want to land here
+elseif( $xmlContent = Get-Content -Path "$xmlResultPath\$resultxml" ) { return $xmlContent }
+
+# Throw an unknown error
+else { Set-ErrorXML "Unbekannter Fehler im Script $($MyInvocation.InvocationName)" }
+
+# Close the PS-Drive if it was used
+If (Test-Path PS-VeeamResults) { Remove-PSDrive -Name PS-VeeamResults }
---
+++ VeeamM365JobCheck-XML.ps1
@@ -0,0 +1,304 @@
+﻿<#
+    .SYNOPSIS
+    This script checks the status of all active Veeam for Microsoft 365 Jobs on a backup server.
+    It collects detailed information and creates an XML file per backupjob as output.
+
+    .INPUTS
+    None
+
+    .OUTPUTS
+    The script creates a XML file formated for PRTG.
+
+    .LINK
+    https://raw.githubusercontent.com/tn-ict/Public/master/Disclaimer/DISCLAIMER
+
+    .NOTES
+    Author  : Andreas Bucher
+    Version : 0.9.1
+    Purpose : XML-Part of the PRTG-Sensor VeeamM365JobCheck
+
+    .EXAMPLE
+    Run this script with task scheduler use powershell.exe as program and the following parameters:
+    -NoProfile -NoLogo -NonInteractive -ExecutionPolicy Bypass -File "C:\Script\VeeamM365JobCheck-XML.ps1"
+    This will place a file in C:\Temp\VeeamResults where it can be retreived by the PRTG sensor
+#>
+#----------------------------------------------------------[Declarations]----------------------------------------------------------
+# Use TLS1.2 for Invoke-Webrequest
+[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
+
+# General parameters
+$UpdatePath       = "https://raw.githubusercontent.com/buesche87/PRTG.Veeam365JobCheck/main/Check-Veeam365Jobs-XML.ps1"
+$nl               = [Environment]::NewLine
+$resultFolder     = "C:\Temp\VeeamResults"
+
+# PRTG parameters
+$WarningLevel = 24 # Warninglevel in hours for last backup session
+$ErrorLevel   = 36 # Errorlevel in hours for last backup session
+
+# Define JobResult object and parameters
+$JobResult = [PSCustomObject]@{
+    Name      = ""
+    Value     = 0
+    Text      = ""
+    Warning   = 0
+    Error     = 0
+    CountObj  = 0
+    ProcItems = 0
+    ProcRate  = 0
+    ReadRate  = 0
+    WriteRate = 0
+    TransData = 0
+    Duration  = 0
+    LastBkp   = 0
+}
+
+#-----------------------------------------------------------[Functions]------------------------------------------------------------
+# Export XML
+function Set-XMLContent {
+    param(
+        $JobResult
+    )
+
+    # Create XML-Content
+    $result= ""
+    $result+= '<?xml version="1.0" encoding="UTF-8" ?>' + $nl
+    $result+= "<prtg>" + $nl
+
+    $result+=   "<Error>$($JobResult.Error)</Error>" + $nl
+    $result+=   "<Text>$($JobResult.Text)</Text>" + $nl
+
+    $result+=   "<result>" + $nl
+    $result+=   "  <channel>Status</channel>" + $nl
+    $result+=   "  <value>$($JobResult.Value)</value>" + $nl
+    $result+=   "  <Warning>$($JobResult.Warning)</Warning>" + $nl
+    $result+=   "  <LimitMaxWarning>2</LimitMaxWarning>" + $nl
+    $result+=   "  <LimitMaxError>3</LimitMaxError>" + $nl
+    $result+=   "  <LimitMode>1</LimitMode>" + $nl
+    $result+=   "  <showChart>1</showChart>" + $nl
+    $result+=   "  <showTable>1</showTable>" + $nl
+    $result+=   "</result>" + $nl
+
+    $result+=   "<result>" + $nl
+    $result+=   "  <channel>Abgearbeitete Objekte</channel>" + $nl
+    $result+=   "  <value>$($JobResult.CountObj)</value>" + $nl
+    $result+=   "  <showChart>1</showChart>" + $nl
+    $result+=   "  <showTable>1</showTable>" + $nl
+    $result+=   "</result>" + $nl
+
+    $result+=   "<result>" + $nl
+    $result+=   "  <channel>Items/s</channel>" + $nl
+    $result+=   "  <value>$($JobResult.ProcItems)</value>" + $nl
+    $result+=   "  <showChart>1</showChart>" + $nl
+    $result+=   "  <showTable>1</showTable>" + $nl
+    $result+=   "</result>" + $nl
+
+    $result+=   "<result>" + $nl
+    $result+=   "  <channel>Processing Rate</channel>" + $nl
+    $result+=   "  <value>$($JobResult.ProcRate)</value>" + $nl
+    $result+=   "  <Float>1</Float>" + $nl
+    $result+=   "  <CustomUnit>MB/s</CustomUnit>" + $nl
+    $result+=   "  <showChart>1</showChart>" + $nl
+    $result+=   "  <showTable>1</showTable>" + $nl
+    $result+=   "</result>" + $nl
+
+    $result+=   "<result>" + $nl
+    $result+=   "  <channel>Read Rate</channel>" + $nl
+    $result+=   "  <value>$($JobResult.ReadRate)</value>" + $nl
+    $result+=   "  <Float>1</Float>" + $nl
+    $result+=   "  <CustomUnit>MB/s</CustomUnit>" + $nl
+    $result+=   "  <showChart>1</showChart>" + $nl
+    $result+=   "  <showTable>1</showTable>" + $nl
+    $result+=   "</result>" + $nl
+
+    $result+=   "<result>" + $nl
+    $result+=   "  <channel>Write Rate</channel>" + $nl
+    $result+=   "  <value>$($JobResult.WriteRate)</value>" + $nl
+    $result+=   "  <Float>1</Float>" + $nl
+    $result+=   "  <CustomUnit>MB/s</CustomUnit>" + $nl
+    $result+=   "  <showChart>1</showChart>" + $nl
+    $result+=   "  <showTable>1</showTable>" + $nl
+    $result+=   "</result>" + $nl
+
+    $result+=   "<result>" + $nl
+    $result+=   "  <channel>Transferiert</channel>" + $nl
+    $result+=   "  <value>$($JobResult.TransData)</value>" + $nl
+    $result+=   "  <Float>1</Float>" + $nl
+    $result+=   "  <CustomUnit>MB</CustomUnit>" + $nl
+    $result+=   "  <showChart>1</showChart>" + $nl
+    $result+=   "  <showTable>1</showTable>" + $nl
+    $result+=   "</result>" + $nl
+
+    $result+=   "<result>" + $nl
+    $result+=   "  <channel>Dauer</channel>" + $nl
+    $result+=   "  <value>$($JobResult.Duration)</value>" + $nl
+    $result+=   "  <Float>1</Float>" + $nl
+    $result+=   "  <CustomUnit>Min</CustomUnit>" + $nl
+    $result+=   "  <showChart>1</showChart>" + $nl
+    $result+=   "  <showTable>1</showTable>" + $nl
+    $result+=   "</result>" + $nl
+
+    $result+=   "<result>" + $nl
+    $result+=   "  <channel>Stunden seit letzem Job</channel>" + $nl
+    $result+=   "  <value>$($JobResult.LastBkp)</value>" + $nl
+    $result+=   "  <CustomUnit>h</CustomUnit>" + $nl
+    $result+=   "  <showChart>1</showChart>" + $nl
+    $result+=   "  <showTable>1</showTable>" + $nl
+    $result+=   "  <LimitMaxWarning>$WarningLevel</LimitMaxWarning>" + $nl
+    $result+=   "  <LimitWarningMsg>Backup-Job &#228;lter als 24h</LimitWarningMsg>" + $nl
+    $result+=   "  <LimitMaxError>$ErrorLevel</LimitMaxError>" + $nl
+    $result+=   "  <LimitErrorMsg>Backup-Job &#228;lter als 36h</LimitErrorMsg>" + $nl
+    $result+=   "  <LimitMode>1</LimitMode>" + $nl
+    $result+=   "</result>" + $nl
+
+    $result+= "</prtg>" + $nl
+
+    # Write XML-File
+    if(-not (test-path $resultFolder)){ New-Item -Path $resultFolder -ItemType Directory }
+    $xmlFilePath = "$resultFolder\$($JobResult.Name).xml"
+    $result | Out-File $xmlFilePath -Encoding utf8
+
+}
+# Calculate Backup job details
+function Get-JobResult {
+    param(
+        $Session
+    )
+
+    # Get session processed objects and duration
+    $JobResult.CountObj = $Session.Statistics.ProcessedObjects
+    $JobResult.Duration = [Math]::Round(($Session.EndTime - $Session.CreationTime).TotalMinutes, 2)
+
+    # Get session status
+    if     ($Session.Status -eq "Success")        { $JobResult.Value = 1; $JobResult.Warning = 0; $JobResult.Error = 0; $JobResult.Text = "BackupJob $($JobResult.Name) erfolgreich" }
+    elseif ($Session.Status -eq "Warning")        { $JobResult.Value = 2; $JobResult.Warning = 1; $JobResult.Error = 0; $JobResult.Text = "BackupJob $($JobResult.Name) Warnung. Bitte pr&#252;fen" }
+    elseif ($Session.Status -eq "Failed")         { $JobResult.Value = 3; $JobResult.Warning = 0; $JobResult.Error = 1; $JobResult.Text = "BackupJob $($JobResult.Name) fehlerhaft" }
+    elseif ($Session.Status -eq "Running")        { $JobResult.Value = 2; $JobResult.Warning = 1; $JobResult.Error = 0; $JobResult.Text = "BackupJob $($JobResult.Name) l&#228;uft noch"  }
+    elseif ($Session.Status -eq "Disconnected")   { $JobResult.Value = 3; $JobResult.Warning = 0; $JobResult.Error = 1; $JobResult.Text = "BackupJob $($JobResult.Name) disconnected" }
+    elseif ($Session.Status -eq "Stopped")        { $JobResult.Value = 1; $JobResult.Warning = 0; $JobResult.Error = 0; $JobResult.Text = "BackupJob $($JobResult.Name) gestoppt" }
+    elseif ($Session.Status -eq "Queued")         { $JobResult.Value = 1; $JobResult.Warning = 0; $JobResult.Error = 0; $JobResult.Text = "BackupJob $($JobResult.Name) eingereiht" }
+    elseif ($Session.Status -eq "NotConfigured")  { $JobResult.Value = 3; $JobResult.Warning = 0; $JobResult.Error = 1; $JobResult.Text = "BackupJob $($JobResult.Name) nicht konfiguriert" }
+    else                                          { $JobResult.Value = 3; $JobResult.Warning = 0; $JobResult.Error = 1; $JobResult.Text = "BackupJob $($JobResult.Name) unbekannter Fehler" }
+
+    Return $JobResult
+}
+# Get Job Statistics
+function Get-Statistics {
+    param(
+        $Statistic
+    )
+
+    # Get item per second
+    $JobResult.ProcItems = $procrate[2] -replace '[()]',''
+
+    # Check processing speed and output it as MB/s
+    $procrate = $Session.Statistics.ProcessingRate -split '\s+'
+    if ($procrate[1] -eq "B/s")  { $JobResult.ProcRate = [Math]::Round([Decimal]$procrate[0]/1MB, 2) }
+    if ($procrate[1] -eq "KB/s") { $JobResult.ProcRate = [Math]::Round([Decimal]$procrate[0]/1KB, 2) }
+    if ($procrate[1] -eq "MB/s") { $JobResult.ProcRate = [Math]::Round([Decimal]$procrate[0], 2) }
+
+    # Check read speed and output it as MB/s
+    $readrate = $Session.Statistics.ReadRate -split '\s+'
+    if ($readrate[1] -eq "B/s")  { $JobResult.ReadRate = [Math]::Round([Decimal]$readrate[0]/1MB, 2) }
+    if ($readrate[1] -eq "KB/s") { $JobResult.ReadRate = [Math]::Round([Decimal]$readrate[0]/1KB, 2) }
+    if ($readrate[1] -eq "MB/s") { $JobResult.ReadRate = [Math]::Round([Decimal]$readrate[0], 2) }
+
+    # Check write speed and output it as MB/s
+    $writerate = $Session.Statistics.WriteRate -split '\s+'
+    if ($writerate[1] -eq "B/s")  { $JobResult.WriteRate = [Math]::Round([Decimal]$writerate[0]/1MB, 2) }
+    if ($writerate[1] -eq "KB/s") { $JobResult.WriteRate = [Math]::Round([Decimal]$writerate[0]/1KB, 2) }
+    if ($writerate[1] -eq "MB/s") { $JobResult.WriteRate = [Math]::Round([Decimal]$writerate[0], 2) }
+
+    # # Check transfered speed and output it as MB/s
+    $transfered = $Session.Statistics.TransferredData -split '\s+'
+    if ($transfered[1] -eq "B")  { $JobResult.TransData = [Math]::Round([Decimal]$transfered[0]/1MB, 2) }
+    if ($transfered[1] -eq "KB") { $JobResult.TransData = [Math]::Round([Decimal]$transfered[0]/1KB, 2) }
+    if ($transfered[1] -eq "MB") { $JobResult.TransData = [Math]::Round([Decimal]$transfered[0], 2) }
+    if ($transfered[1] -eq "GB") { $JobResult.TransData = [Math]::Round([Decimal]$transfered[0]*1KB, 2) }
+
+    Return $JobResult
+}
+# Get Logs for Tape, Agent and Copy-Jobs
+function Get-JobLog {
+    param(
+        $Session
+    )
+
+    # Find warning and error messages in session log
+    $warningmsg = ""
+    $failedmsg  = ""
+    $warningmsg = $Session.Log | Where-Object {$_.Title -like "*Warning*"} | ForEach-Object { $_.title }
+    $failedmsg  = $Session.Log | Where-Object {$_.Title -like "*Failed*"} | ForEach-Object { $_.title }
+
+    if ($failedmsg)      { Return $failedmsg }
+    elseif ($warningmsg) { Return $warningmsg }
+    else                 { Return }
+}
+# Update Script
+function Get-NewScript {
+
+    # Check if Update-Script is reachable
+    $StatusCode = Invoke-WebRequest $UpdatePath -UseBasicParsing | ForEach-Object {$_.StatusCode}
+    $CurrentScript = $PSCommandPath
+
+    if ($StatusCode -eq 200 ) {
+
+        # Parse version string of script on github
+        $UpdateScriptcontent = (Invoke-webrequest -URI $UpdatePath -UseBasicParsing).Content
+        $newversionstring    = ($UpdateScriptcontent | Select-String "Version :.*" | Select-Object -First 1).Matches.Value
+        $newversion          = $newversionstring -replace '[^0-9"."]',''
+
+        # Parse version string of current script
+        $CurrentScriptContent = Get-Content -Path $PSCommandPath -Encoding UTF8 -Raw
+        $currentversionstring = ($CurrentScriptContent | Select-String "Version :.*" | Select-Object -First 1).Matches.Value
+        $currentversion       = $currentversionstring -replace '[^0-9"."]',''
+
+        # Replace and re-run script if update-script is newer
+        if ([version]$newversion -gt [version]$currentversion) {
+
+            # Create temp directory if it does not exists
+            $tmpdirectory = "C:\Temp"
+            if(-not (test-path $tmpdirectory)){ New-Item -Path $tmpdirectory -ItemType Directory }
+
+            # Create a temporary file with content of the new script
+            $tempfile = "$tmpdirectory\update-script.new"
+            Invoke-WebRequest -URI $UpdatePath -outfile $tempfile
+
+            # Replace current script
+            $content = Get-Content $tempfile -Encoding utf8 -raw
+            $content | Set-Content $CurrentScript -encoding UTF8
+
+            # Remove temporary file
+            Remove-Item $tempfile
+
+            # Call new script
+            &$CurrentScript $script:args
+        }
+    }
+}
+#-----------------------------------------------------------[Execute]------------------------------------------------------------
+# Autouptade Script
+Get-NewScript
+
+# Get M365 BackupJobs
+$BackupJobs = Get-VBOJob | where-object { $_.IsEnabled -eq $True }
+
+#### Get BackupJob details ######################################################################################################
+foreach($item in $BackupJobs) {
+
+    $JobResult.Name = $item.Name
+
+    # Load last session
+    $Session = Get-VBOJobSession | Where-Object { ( $_.JobName -like $JobResult.Name ) } | Sort-Object -Property Creationtime -Descending | Select-Object -First 1
+
+    # Check job results
+    $JobResult = Get-JobResult $Session
+    $JobResult = Get-Statistics $Session
+    $JobResult.LastBkp = (New-TimeSpan -Start $Session.CreationTime -End (Get-Date)).Hours
+
+    # Check for messages in session log
+    $CheckJobError = Get-JobLog $Session
+    if ($CheckJobError) { $JobResult.Text = $CheckJobError }
+
+    # Create XML 
+    Set-XMLContent -JobResult $JobResult -HoursSince $HoursSince
+}
